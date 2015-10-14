997fb28786f8fa74e3d417f2004d3a1352b9c0da
Roizus <alvaroroizo@gmail.com>
2015-10-14 18:23:41 +0200

    Primera version rama_alvaro

--- src/ManejoFicheros.java
+++ src/ManejoFicheros.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+
+
+import javax.sound.midi.Patch;
 
 
 public class ManejoFicheros implements InterfazManejoFicheros {
@@ -18,8 +26,11 @@ public class ManejoFicheros implements InterfazManejoFicheros {
 
 	@Override//Metodo3
 	public boolean puedeEjecutarFichero(Path patch) {
-		// TODO Apéndice de método generado automáticamente
-		return false;
+		//Directamente devuelve un booleano true si es ejecutable, false si no es ejecutable. 
+		//Aunque el patch no fuese valido el resultado seria false pero no saltarian excepciones.
+		System.out.println("\nMetodo 3\n");
+		System.out.println(Files.isExecutable(patch));
+		return  Files.isExecutable(patch);
 	}
 
 	@Override//Metodo4
@@ -48,8 +59,27 @@ public class ManejoFicheros implements InterfazManejoFicheros {
 
 	@Override//Metodo8
 	public String[] listarModificads(Path patch, FileTime file) {
-		// TODO Apéndice de método generado automáticamente
-		return null;
+		//Recorremos el directorio actual comparando cada fecha de ultima modificacion con la fecha dada.
+		System.out.println("\nMetodo 8\n");
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(patch)) {
+            ArrayList<String> EncontradosAL = new ArrayList<String>();
+     
+            for (Path i : stream) {
+                FileTime fileTime = Files.getLastModifiedTime(i);
+                if(fileTime.compareTo(file)>=0){
+                	System.out.println(i.getFileName().toString());
+                    EncontradosAL.add(i.getFileName().toString());
+                }
+            }
+            String []Encontrados = new String[EncontradosAL.size()];
+            EncontradosAL.toArray(Encontrados);
+            return Encontrados;
+            
+        } //En caso de que salte un excepción devolveria Null
+        catch (IOException | DirectoryIteratorException e) {
+            System.err.println(e);
+            return null;
+        }
 	}
 
 	@Override//Metodo9
